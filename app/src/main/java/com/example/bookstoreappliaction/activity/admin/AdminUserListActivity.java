86d5677bd80e228c3ed1012e46375ee76bc71f0a
Nguyễn Vương Bách <nguyenvuongbach933@gmail.com>
2023-07-09 18:46:17 +0700

    Add navigation admin (#3)
    * update admin screen
    
    * fix admin
    
    * Add navigation admin

--- app/src/main/java/com/example/bookstoreappliaction/activity/admin/AdminUserListActivity.java
+++ app/src/main/java/com/example/bookstoreappliaction/activity/admin/AdminUserListActivity.java
@@ -1,10 +1,18 @@
 package com.example.bookstoreappliaction.activity.admin;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.bookstoreappliaction.R;
+import com.example.bookstoreappliaction.activity.login.LoginActivity;
 import android.os.Bundle;
 
 import com.example.bookstoreappliaction.R;
@@ -16,9 +24,11 @@ import com.example.bookstoreappliaction.models.User;
 
 import java.util.List;
 
-public class AdminUserListActivity extends AppCompatActivity {
+public class AdminUserListActivity extends AppCompatActivity implements View.OnClickListener {
     RecyclerView rvUserList;
     List<User> users;
+    ImageButton btnDashboard, btnProducts, btnLogOut;
+    Intent intent;
     BookStoreDb db;
     UserAdapter adapter;
 
@@ -28,6 +38,14 @@ public class AdminUserListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_user_list);
 
         rvUserList = findViewById(R.id.activity_adminuserlist_rv_userList);
+
+        btnDashboard = findViewById(R.id.btnDashboard);
+        btnProducts = findViewById(R.id.btnProducts);
+        btnLogOut = findViewById(R.id.btnLogout);
+
+        btnDashboard.setOnClickListener(this);
+        btnProducts.setOnClickListener(this);
+        btnLogOut.setOnClickListener(this);
         db = Room.databaseBuilder(getApplicationContext(),
                 BookStoreDb.class, Constants.DB_NAME).build();
         LoadList();
@@ -44,5 +62,27 @@ public class AdminUserListActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnDashboard){
+            intent = new Intent(AdminUserListActivity.this, AdminDashboardActivity.class);
+            startActivity(intent);
+        }else if(v.getId() == R.id.btnProducts){
+            intent = new Intent(AdminUserListActivity.this, AdminBookListActivity.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.btnLogout) {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Log Out")
+                    .setMessage("Are you sure you want to log out?")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        intent = new Intent(AdminUserListActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
     }
 }
